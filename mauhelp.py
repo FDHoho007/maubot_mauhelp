98d57bfe0e93187499606be40218f4adf3be4123
FDHoho007 <mail@fdhoho007.de>
2025-11-23 12:24:21 +0100

    Cleanup code

--- mauhelp.py
+++ mauhelp.py
@@ -7,24 +7,20 @@ class MauHelp(Plugin):
     @command.new(name="help", help="Shows this help text")
     async def help(self, evt: MessageEvent):
         help_text_dict = {}
-        for handler_func in self.client.event_handlers[EventType.ROOM_MESSAGE]:
-            command_help = ""
-            if hasattr(handler_func, "__mb_name__") and hasattr(handler_func, "__mb_usage_inline__"):
-                # Initialize the key if it doesn't exist
-                if handler_func.__mb_name__ not in help_text_dict:
-                    help_text_dict[handler_func.__mb_name__] = ""
+        for command_handler in self.client.event_handlers[EventType.ROOM_MESSAGE]:
+            if hasattr(command_handler, "__mb_name__") and hasattr(command_handler, "__mb_usage_inline__"):
+                help_text = command_handler.__mb_usage_without_subcommands__
 
-                if handler_func.__mb_help__ is not None:
-                    command_help = f" - {handler_func.__mb_help__}"
-                help_text_dict[handler_func.__mb_name__] += f"{handler_func.__mb_usage_without_subcommands__}{command_help}\n"
+                if command_handler.__mb_help__ is not None:
+                    help_text += f" - {command_handler.__mb_help__}"
+                
+                help_text += "\n"
+                
+                if not command_handler.__mb_require_subcommand__:
+                    help_text += f"* {command_handler.__mb_prefix__} {command_handler.__mb_usage_args__} - {command_handler.__mb_help__}\n"
+                
+                help_text += "\n".join(cmd.__mb_usage_inline__ for cmd in command_handler.__mb_subcommands__) + "\n"
+                
+                help_text_dict[command_handler.__mb_name__] = help_text 
 
-                if not handler_func.__mb_require_subcommand__:
-                    help_text_dict[handler_func.__mb_name__] += f"* {handler_func.__mb_prefix__} {handler_func.__mb_usage_args__} - {handler_func.__mb_help__}\n"
-
-                help_text_dict[handler_func.__mb_name__] += "\n".join(cmd.__mb_usage_inline__ for cmd in handler_func.__mb_subcommands__) + "\n"
-
-        help_text = ""
-        for plugin in help_text_dict:
-            help_text += f"\n{help_text_dict[plugin]}\n"
-
-        await evt.reply(help_text)
+        await evt.reply("\n\n".join(help_text_dict.values()))
